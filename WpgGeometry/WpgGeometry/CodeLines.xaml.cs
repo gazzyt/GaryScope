e1b39e2f56847ce052ef0d48fee0d709a581e316
Gary Thomas <gary.thomas@ioko.com>
2013-01-22 18:50:04 +0000

    use ring buffer

--- WpgGeometry/WpgGeometry/CodeLines.xaml.cs
+++ WpgGeometry/WpgGeometry/CodeLines.xaml.cs
@@ -21,10 +21,12 @@ namespace WpgGeometry
     public partial class CodeLines : Window
     {
         private double phase = 0.0;
-        private int sample = 0;
-        private Timer timer;
+        private int nextSample = 0;
+        private Timer sampleTimer;
+        private Timer redrawTimer;
         private GeometryGroup lines;
         private const int linesPerTrace = 100;
+        private double[] samples = new double[linesPerTrace];
 
         public CodeLines()
         {
@@ -33,10 +35,31 @@ namespace WpgGeometry
 
         private void canvas_Loaded(object sender, RoutedEventArgs e)
         {
+            do
+            {
+                AddSample();
+            } while (nextSample != 0);
+
+            DrawScene();
+
+
+            sampleTimer = new Timer(SampleTimerTick, null, 0, 10);
+            redrawTimer = new Timer(RedrawTimerTick, null, 0, 50);
+        }
+
+        private void RedrawTimerTick(object state = null)
+        {
+            this.Dispatcher.BeginInvoke(
+                new Action(() => this.DrawScene()));
+        }
+        private void DrawScene()
+        {
             // 
             // Create the Geometry to draw. 
             //
             lines = new GeometryGroup();
+
+
             AddLines(lines);
             //lines.Children.Add(
             //    new EllipseGeometry(new Point(50, 50), 45, 20)
@@ -81,37 +104,36 @@ namespace WpgGeometry
             // Place the image inside a border and 
             // add it to the page. 
             //
-/*            Border exampleBorder = new Border();
-            exampleBorder.Child = anImage;
-            exampleBorder.BorderBrush = Brushes.Gray;
-            exampleBorder.BorderThickness = new Thickness(1);
-            exampleBorder.HorizontalAlignment = HorizontalAlignment.Left;
-            exampleBorder.VerticalAlignment = VerticalAlignment.Top;
-            exampleBorder.Margin = new Thickness(10);
+            /*            Border exampleBorder = new Border();
+                        exampleBorder.Child = anImage;
+                        exampleBorder.BorderBrush = Brushes.Gray;
+                        exampleBorder.BorderThickness = new Thickness(1);
+                        exampleBorder.HorizontalAlignment = HorizontalAlignment.Left;
+                        exampleBorder.VerticalAlignment = VerticalAlignment.Top;
+                        exampleBorder.Margin = new Thickness(10);
 
-            this.Margin = new Thickness(20);
-            this.Background = Brushes.White;
-            this.Content = exampleBorder; */
+                        this.Margin = new Thickness(20);
+                        this.Background = Brushes.White;
+                        this.Content = exampleBorder; */
 
+            canvas.Children.Clear();
             canvas.Children.Add(anImage);
-
-            timer = new Timer(TimerTick, null, 0, 10);
         }
 
         private void AddLines(GeometryGroup group)
         {
-            int lineNum;
             Point lastLineEnd = new Point(0.0, 0.0);
-
-            for (phase = 0.0; phase < linesPerTrace; phase++)
+            int sample = GetPreviousSample(nextSample);
+            for (int s = 0; s < linesPerTrace; s++)
             {
-                Point thisLineEnd = new Point(phase * 5, Math.Sin(phase/40.0) * 100);
+                Point thisLineEnd = new Point(s * 5, samples[sample]);
                 group.Children.Add(new LineGeometry(lastLineEnd, thisLineEnd));
                 lastLineEnd = thisLineEnd;
+                sample = GetPreviousSample(sample);
             }
         }
 
-        private void TimerTick(object state)
+        private void SampleTimerTick(object state)
         {
             this.Dispatcher.Invoke(
                 new Action(() => AddSample()));
@@ -119,15 +141,22 @@ namespace WpgGeometry
 
         private void AddSample()
         {
-            LineGeometry oldLine = (LineGeometry)lines.Children[sample];
-            LineGeometry prevLine = (LineGeometry)lines.Children[sample > 0 ? sample - 1 : linesPerTrace-1];
-            oldLine.StartPoint = new Point(oldLine.StartPoint.X, prevLine.EndPoint.Y);
-            oldLine.EndPoint = new Point(oldLine.EndPoint.X, Math.Sin(phase++ / 10.0) * 100);
-            sample++;
-            if (sample >= linesPerTrace)
+            samples[nextSample] = Math.Sin(phase++ / 10.0) * 100;
+            nextSample++;
+            if (nextSample >= linesPerTrace)
             {
-                sample = 0;
+                nextSample = 0;
             }
+        }
+
+        private static int GetPreviousSample(int nextSample)
+        {
+            int retval = nextSample - 1;
+            if (retval < 0)
+            {
+                retval = linesPerTrace - 1;
+            }
+            return retval;
         }
     }
 }
