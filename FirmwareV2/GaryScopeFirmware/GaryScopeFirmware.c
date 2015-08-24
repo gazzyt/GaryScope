f130a0152478c22230fd911aef5c6aefb6e20163
Gary <gazzyt@live.co.uk>
2015-08-24 15:26:46 +0100

    Added new v2 firmware skeleton project

---
+++ FirmwareV2/GaryScopeFirmware/GaryScopeFirmware.c
@@ -0,0 +1,19 @@
+/*
+ * GaryScopeFirmware.c
+ *
+ * Created: 24/08/2015 13:00:07
+ *  Author: garyt
+ */ 
+
+
+#include <avr/io.h>
+
+#include "usbdrv.h"
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
